82e651461e3023a94f43ede477c59efd18e20303
Muhammad Idrees <midrees097@gmail.com>
2020-12-21 00:05:47 +0500

    trying to solve the country data issue

--- code/data_preparation.py
+++ code/data_preparation.py
@@ -35,10 +35,40 @@ covid["ObservationDate"] = pd.to_datetime(covid["ObservationDate"])
 print("\nAfter formatting observation date:\n")
 print(covid.head())
 
-# grouped_country=covid.groupby(["Country/Region"]).agg({"Confirmed":'sum',"Recovered":'sum',"Deaths":'sum'})
+grouped_country=covid.groupby(["Country/Region"], as_index=False).agg({"Confirmed":'sum',"Recovered":'sum',"Deaths":'sum'})
 
-# print("\nAfter formatting observation date:\n")
-# print(grouped_country)
-# print(grouped_country.shape)
+print("\nAfter formatting observation date:\n")
+print(grouped_country.head())
+print(grouped_country.shape)
 
-# print(len(covid["Country/Region"].str()))
+list_of_countries = [country.name for country in list(pycountry.countries)]
+wrong_names = []
+r = 0
+def get_wrong_countries(row):
+    global r
+    for country in list_of_countries:
+        if country in row["Country/Region"]:
+            r = r + 1
+            return country
+    wrong_names.append(row["Country/Region"])    
+    return 
+    # return row['height'] * row['width']
+
+print(grouped_country.apply(get_wrong_countries, axis=1))
+print(wrong_names)
+
+print()
+print(r)
+print()
+print()
+print(list_of_countries)
+
+# print("\nNull values in columns:\n", grouped_country.)
+# print(covid["Country/Region"].str.len())
+# print(covid.groupby("Country/Region", as_index=False).apply(lambda x: x[x["Country/Region"].str.len() == x["Country/Region"].str.len().max()]))
+
+# country_len = covid["Country/Region"].str.len()
+
+# print(covid.pivot_table(index = ["Country/Region"], aggfunc ='size'))
+# dups = df.pivot_table(index = ['Course'], aggfunc ='size')
+# print(len(pycountry.countries)) 
